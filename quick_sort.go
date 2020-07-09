9e37d33eb11348087acd665254485b61a7b1b3b9
asmlv <asmlv@protonmail.com>
2020-07-09 12:57:39 +0300

    Improvements, refactoring and comments

--- quick_sort.go
+++ quick_sort.go
@@ -5,39 +5,47 @@ import (
 	"time"
 )
 
-var _predicate Order = Less
-
-func getRandomIndex(left, right int) int {
-	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(right-left) + left
-}
-
-func getPartitionIndex(arr []int, left, right int) int {
-	partitionIndex := left
-	randomIndex := getRandomIndex(left, right)
-	arr[randomIndex], arr[right] = arr[right], arr[randomIndex]
-	// last element is a base element (index == right)
-	for current := left; current < right; current++ {
-		if _predicate(arr[current], arr[right]) {
-			arr[partitionIndex], arr[current] = arr[current], arr[partitionIndex]
-			partitionIndex++
-		}
-	}
-	arr[partitionIndex], arr[right] = arr[right], arr[partitionIndex]
-	return partitionIndex
-}
-
-func quickSort(arr []int, left, right int) {
-	if left >= right {
-		return
-	}
-	partitionIndex := getPartitionIndex(arr, left, right)
-	quickSort(arr, left, partitionIndex-1)
-	quickSort(arr, partitionIndex+1, right)
-}
-
 // QuickSort algorithm realization
 func QuickSort(arr []int, predicate Order) {
-	_predicate = predicate
-	quickSort(arr, 0, len(arr)-1)
+	// Enable random generator
+	rand.Seed(time.Now().UnixNano())
+
+	// If there are 1 or 0 elements, the array is considered to be sorted
+	if len(arr) < 2 {
+		return
+	}
+
+	// Borders
+	left, right := 0, len(arr)-1
+
+	// Choose a pivot
+	pivotIndex := rand.Int() % len(arr)
+
+	// Swap the pivot element with the last
+	// (because the last element is a base element)
+	arr[pivotIndex], arr[right] = arr[right], arr[pivotIndex]
+
+	// Lesser area = [0, left - 1]
+	// Greater area = [left, i - 1]
+	// Undefined area = [i, right - 1]
+	// Pivot element = arr[right]
+	for i := range arr {
+		if predicate(arr[i], arr[right]) {
+			arr[i], arr[left] = arr[left], arr[i]
+			left++
+		}
+	}
+
+	// Place the pivot element after the last smallest element,
+	// because:
+	//    - in area [0, left-1] are elements, smaller than the pivot,
+	//    - in area [left, right - 1] are elements, greater than the pivot,
+	//    - in [right] is a pivot element.
+	// So we need to place the last element between them, making the folowing structure:
+	//    smaller + pivot + greater
+	arr[left], arr[right] = arr[right], arr[left]
+
+	// After that, we need to sort smaller elements and greater elements including pivot
+	QuickSort(arr[:left], predicate)
+	QuickSort(arr[left:], predicate)
 }
